038bbfd1094db73c2b5b3d43c6260d5295a03ebf
Harshit Rai <raiharshit66@gmail.com>
2025-03-08 02:05:03 +0530

    Import the Canva Images in the main.py using os

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ import HandTrackingModule as hm
 import os
 import math
 
+directory = "CanvaImage"
+myList  = os.listdir(directory)
+print(myList)
+headerImages = []
+for imgPath in myList:
+    img = cv2.imread(f'{directory}/{imgPath}')
+    headerImages.append(img)
+ 
