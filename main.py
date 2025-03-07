7aed28a89f6c0a148fef30ce29069a8f1801c2db
Harshit Rai <raiharshit66@gmail.com>
2025-03-08 02:08:12 +0530

    Sample test the samera feed

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ headerImages = []
 for imgPath in myList:
     img = cv2.imread(f'{directory}/{imgPath}')
     headerImages.append(img)
- 
+currHeader = headerImages[0]
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+while True:
+    success, img = cap.read()
+    cv2.imshow("Feed", img)
+    cv2.waitKey(1)
