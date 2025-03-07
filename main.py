c385c2c7c075249d04bef9af91edab948cdc8dcc
Harshit Rai <raiharshit66@gmail.com>
2025-03-08 02:18:25 +0530

    Use the HTM module to track hand and print the landmark Points of hand

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ cap.set(3, 1280)
 cap.set(4, 720)
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
+    #Detect the hands
+    detector = hm.handDetectorObj()
+    img = detector.detectHands(img)
+    lmrkArray = detector.findHandPosition(img , toDraw=False)
+    if(len(lmrkArray) != 0):
+        print(lmrkArray)
     #set the header image
     img[0:125, 0:1280] = currHeader
     cv2.imshow("Feed", img)
