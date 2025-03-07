e92ea6bf8de7cc56c130420f887f4a646e9ed46f
Harshit Rai <raiharshit66@gmail.com>
2025-03-08 01:15:54 +0530

    Calculate the FPS using the Current time and prevtime in each itertion of loop

--- HandTracking.py
+++ HandTracking.py
@@ -7,6 +7,8 @@ cap = cv2.VideoCapture(0)
 handObj = mp.solutions.hands
 hands = handObj.Hands()
 drawObj  = mp.solutions.drawing_utils
+
+prevTime = 0
 while True:
     success, img = cap.read()
     imRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -15,5 +17,8 @@ while True:
     if result.multi_hand_landmarks:
         for hand in result.multi_hand_landmarks: #For each hand detected
             drawObj.draw_landmarks(img, hand, handObj.HAND_CONNECTIONS)
+    curTime = time.time()
+    fps = 1/(curTime-prevTime)
+    prevTime = curTime
     cv2.imshow("Image", img) 
     cv2.waitKey(1)
