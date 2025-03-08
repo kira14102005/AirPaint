be656c60b5c78c4d5821e172fb038a406a0eea11
Harshit Rai <raiharshit66@gmail.com>
2025-03-09 00:59:09 +0530

    Add one script to do the same using iruinWebcam

---
+++ iruinWebCam.py
@@ -0,0 +1,97 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as hm
+import os
+import numpy as np
+import math
+#########
+brushWeight = 15
+prev_x, prev_y = 0, 0
+eraseThickness = 50
+#########
+imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+directory = "CanvaImage\Header"
+myList  = os.listdir(directory)
+print(myList)
+headerImages = []
+for imgPath in myList:
+    img = cv2.imread(f'{directory}/{imgPath}')
+    headerImages.append(img)
+currHeader = headerImages[0]
+drawColor = (0, 0, 255) #By default Red COlor
+
+cap = cv2.VideoCapture(0)  # Iriun Webcam as default camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # Force resolution to 1280x720
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    #Detect the hands
+    detector = hm.handDetectorObj()
+    img = detector.detectHands(img)
+    lmrkArray = detector.findHandPosition(img , toDraw=False)
+    if(len(lmrkArray) != 0):
+        # print(lmrkArray)
+
+        #TIp of the index and middle finger
+        x1, y1 = lmrkArray[8][1], lmrkArray[8][2]
+        x2, y2 = lmrkArray[12][1], lmrkArray[12][2]
+    
+        fingers = detector.noOfFingersUp()
+        print(fingers)
+
+        if(fingers[1] and fingers[2]):
+            prev_x, prev_y = 0, 0
+            #Selection Mode
+            cv2.rectangle(img, (x1,y1-25), (x2,y2+25), drawColor, cv2.FILLED)
+
+            print("Selection Mode")
+            if y1 < 125:
+                if 200 < x1 < 280:
+                    currHeader = headerImages[0]
+                    drawColor = (0, 0, 255) #Red
+                elif 435 < x1 < 510:
+                    currHeader = headerImages[1]
+                    drawColor = (0, 255, 0) #Green
+                elif 635 < x1 < 715:
+                    currHeader = headerImages[2]
+                    drawColor = (0, 255, 255) #yellow
+                elif 850 < x1 < 975:
+                    currHeader = headerImages[3]
+                    drawColor = (255, 0 ,0) #blue
+                elif 1050 < x1 < 1160:
+                    currHeader = headerImages[4]
+                    drawColor = (0,0,0) #Black
+        
+        if(fingers[1] and fingers[2] == False):
+            cv2.circle(img, (x1,y1), 15, drawColor, cv2.FILLED)
+            print("Drawing Mode")
+            if(prev_x == 0 and prev_y == 0):
+                prev_x, prev_y = x1, y1
+            if(drawColor == (0,0,0)):
+                cv2.line(img, (prev_x,prev_y), (x1,y1), drawColor, eraseThickness)
+                cv2.line(imgCanvas, (prev_x,prev_y), (x1,y1), drawColor, eraseThickness)
+            else:
+                cv2.line(img, (prev_x,prev_y), (x1,y1), drawColor, brushWeight)
+                cv2.line(imgCanvas, (prev_x,prev_y), (x1,y1), drawColor, brushWeight)
+            prev_x, prev_y = x1, y1
+
+    gray_img = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)  #Convert the canvas to gray
+    _, imgInv = cv2.threshold(gray_img, 50, 255, cv2.THRESH_BINARY_INV)  #Convert the gray image to inverted image binary
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)  #Convert the binary image to RGB
+    img = cv2.bitwise_and(img, imgInv)   #Add the Black pixels to the image where we drew on the canvas
+    img = cv2.bitwise_or(img, imgCanvas)        #Overlay the color's actual RGB values to the absolute black pixels
+
+    #set the header image
+    img[0:125, 0:1280] = currHeader
+    # img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+    cv2.imshow("Feed", img)
+    cv2.imshow("Canvas", imgCanvas)
+    cv2.waitKey(1)
+
+# RULES : 
+# 1.Selection Mode  : Two Fingers point up
+# 2.Drawing Mode : One Fingers point up
